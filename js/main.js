1aeeccd2b26887f5449bf044a8fa623fc26da728
vandelshtam <mvlju977@gmail.com>
2021-12-26 22:21:17 +0300

    page blog

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ $(function(){
     });
     $('').styler();
 });
+
+  // Initialise Carousel
+const newsCarousel = new Carousel(document.querySelector("#newsCarousel"), {
+    infinite: false,
+    Dots: false,
+    friction: 0.89,
+    slidesToSlide: 2,
+    fill: false,
+  });

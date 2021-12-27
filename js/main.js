79a30f597c2ec272405f1f56c72f6074d6de4a2e
vandelshtam <mvlju977@gmail.com>
2021-12-27 19:14:19 +0300

    commit 11

--- js/main.js
+++ js/main.js
@@ -5,18 +5,14 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false
     });
+    $('.blog_slider_inner, .blog_slider-inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    });
     $('select').styler();
     $('.header_btn-menu').on('click', function(){
         $('.menu ul').slideToggle();
     });
     $('').styler();
 });
-
-  // Initialise Carousel
-const newsCarousel = new Carousel(document.querySelector("#newsCarousel"), {
-    infinite: false,
-    Dots: false,
-    friction: 0.89,
-    slidesToSlide: 2,
-    fill: false,
-  });
